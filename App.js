e86303857ab6dd1e9e2bcc579ba7f6f894990d1b
seth <sebarrett@vt.ewsd.org>
2019-03-20 09:31:44 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.paragraph}>
+                    Change code in the editor and watch it change on your phone!
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#34495e',
+    },
+});
